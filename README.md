0fbfd85b9dacb70cbd853c993ef0f22dcea7eaa8
yukiny0811 <yukiny0811@gmail.com>
2022-12-09 04:28:40 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyCreatives
+
+A description of this package.
---
+++ Sources/SwiftyCreatives/SwiftyCreatives.swift
@@ -0,0 +1,6 @@
+public struct SwiftyCreatives {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
