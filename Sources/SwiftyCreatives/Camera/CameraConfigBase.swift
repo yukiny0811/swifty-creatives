b40de9a8874b93558e70fdc2f0a71c20b41ec27f
yukiny0811 <yukiny0811@gmail.com>
2022-12-15 03:01:29 +0900

    document comment

--- Sources/SwiftyCreatives/Camera/CameraConfigBase.swift
+++ Sources/SwiftyCreatives/Camera/CameraConfigBase.swift
@@ -1,13 +1,24 @@
 //
-//  File.swift
+//  CameraConfig.swift
 //  
 //
 //  Created by Yuki Kuwashima on 2022/12/09.
 //
 
+/// Config for Camera
 public protocol CameraConfigBase {
+    
+    /// field of view in radians
     static var fov: Float { get }
+    
+    /// near clipping distance
     static var near: Float { get }
+    
+    /// far clipping distance
     static var far: Float { get }
+    
+    /// defines how much camera can move along y axis in the screen.
+    /// set this number small to allow the camera to move more freely.
+    /// 0.0...1.0
     static var polarSpacing: Float { get }
 }
