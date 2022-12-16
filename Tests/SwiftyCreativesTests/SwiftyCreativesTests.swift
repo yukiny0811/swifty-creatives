004a666a7f0851b18ed4fbd5850000df2788230e
yukiny0811 <yukiny0811@gmail.com>
2022-12-16 18:23:58 +0900

    fix test

--- Tests/SwiftyCreativesTests/SwiftyCreativesTests.swift
+++ Tests/SwiftyCreativesTests/SwiftyCreativesTests.swift
@@ -6,6 +6,5 @@ final class SwiftyCreativesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftyCreatives().text, "Hello, World!")
     }
 }
