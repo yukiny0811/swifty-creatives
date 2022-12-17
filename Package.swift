35535e757570e8c9f5b132dda4ed34fdc9561779
yukiny0811 <yukiny0811@gmail.com>
2022-12-17 23:19:13 +0900

    resolve github actions error

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftyCreatives",
-            targets: ["SwiftyCreatives"]),
+            targets: ["SwiftyCreatives"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,9 +25,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyCreatives",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]
+        ),
         .testTarget(
             name: "SwiftyCreativesTests",
-            dependencies: ["SwiftyCreatives"]),
+            dependencies: ["SwiftyCreatives"]
+        )
     ]
 )
