1ec6a874781ac5a4b33f355fc28ab5e723245340
yukiny0811 <yukiny0811@gmail.com>
2022-12-26 14:33:26 +0900

    version change

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyCreatives",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v12)
     ],
     products: [
