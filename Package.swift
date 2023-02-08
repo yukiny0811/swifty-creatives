da0e0f04ce2fd7d9d0d46c7636fcd30080dd8165
yukiny0811 <yukiny0811@gmail.com>
2023-02-08 22:45:59 +0900

    update CI

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftyCreatives",
-            targets: ["SwiftyCreatives", "SwiftyCreativesTests"]
+            targets: ["SwiftyCreatives"]
         )
     ],
     dependencies: [],
