8d3b59ecb6a2ffc08a6f3cbe37d0195e0fa4f1ec
yukiny0811 <yukiny0811@gmail.com>
2024-02-03 20:43:03 +0900

    refactor functionBase

--- Sources/SwiftyCreatives/Sketch/FunctionBase/FunctionBase.swift
+++ Sources/SwiftyCreatives/Sketch/FunctionBase/FunctionBase.swift
@@ -12,3 +12,37 @@ public protocol FunctionBase: AnyObject {
     var privateEncoder: SCEncoder? { get set }
     var customMatrix: [f4x4] { get set }
 }
+
+extension FunctionBase {
+    
+    internal func setUniforms(modelPos: f3, modelScale: f3, hasTexture: Bool) {
+        setModelPos(modelPos)
+        setModelScale(modelScale)
+        setHasTexture(hasTexture)
+    }
+    
+    private func setModelPos(_ value: f3) {
+        privateEncoder?.setVertexBytes([value], length: f3.memorySize, index: VertexBufferIndex.ModelPos.rawValue)
+    }
+    
+    private func setModelScale(_ value: f3) {
+        privateEncoder?.setVertexBytes([value], length: f3.memorySize, index: VertexBufferIndex.ModelScale.rawValue)
+    }
+    
+    internal func setVertices(_ value: [f3]) {
+        privateEncoder?.setVertexBytes(value, length: value.count * f3.memorySize, index: VertexBufferIndex.Position.rawValue)
+    }
+    
+    internal func setUVs(_ value: [f2]) {
+        privateEncoder?.setVertexBytes(value, length: value.count * f2.memorySize, index: VertexBufferIndex.UV.rawValue)
+    }
+    
+    internal func setNormals(_ value: [f3]) {
+        privateEncoder?.setVertexBytes(value, length: value.count * f3.memorySize, index: VertexBufferIndex.Normal.rawValue)
+    }
+    
+    private func setHasTexture(_ value: Bool) {
+        privateEncoder?.setFragmentBytes([value], length: Bool.memorySize, index: FragmentBufferIndex.HasTexture.rawValue)
+    }
+    
+}
