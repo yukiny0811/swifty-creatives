4f813cdb59efaf01e53985097c356217b8d4d8e1
yukiny0811 <yukiny0811@gmail.com>
2024-02-04 23:18:25 +0900

    refactor macOS Example Sample10

--- Examples/ExampleMacOSApp/ExampleMacOSApp/Sample10.swift
+++ Examples/ExampleMacOSApp/ExampleMacOSApp/Sample10.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 class Sample10: Sketch {
     
-    let swiftTextObj = Text2D(
+    var swiftTextObj: Text2D = Text2D(
         text: "Swifty\nCreatives",
         fontName: "Avenir-BlackOblique",
         fontSize: 10
@@ -23,7 +23,33 @@ class Sample10: Sketch {
 }
 
 struct Sample10View: View {
+    
+    let sketch = Sample10()
+    
     var body: some View {
-        SketchView(Sample10())
+        SketchView(sketch)
+            .toolbar {
+                ToolbarItem {
+                    Menu("Font") {
+                        ForEach(getAvailableFonts(), id: \.self) { font in
+                            Button(font) {
+                                sketch.swiftTextObj = Text2D(
+                                    text: "Swifty\nCreatives",
+                                    fontName: font,
+                                    fontSize: 10
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+    }
+    
+    func getAvailableFontFamilies() -> [String] {
+        return NSFontManager.shared.availableFontFamilies
+    }
+
+    func getAvailableFonts() -> [String] {
+        return NSFontManager.shared.availableFonts
     }
 }
