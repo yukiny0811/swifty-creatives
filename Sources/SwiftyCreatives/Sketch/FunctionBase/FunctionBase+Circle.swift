6b4b773342a7d9e9e75661b730aebca3c682a6b2
Yuki Kuwashima <yukiny0811@gmail.com>
2024-02-22 11:56:49 +0900

    fix

--- Sources/SwiftyCreatives/Sketch/FunctionBase/FunctionBase+Circle.swift
+++ Sources/SwiftyCreatives/Sketch/FunctionBase/FunctionBase+Circle.swift
@@ -15,9 +15,7 @@ public extension FunctionBase {
         setVertices(CircleInfo.vertices)
         setUVs(CircleInfo.uvs)
         setNormals(CircleInfo.normals)
-        if hasTexture == false {
-            setVertexColors(CircleInfo.vertices.map { f4.zero })
-        }
+        setVertexColors(CircleInfo.vertices.map { f4.zero })
         privateEncoder?.drawIndexedPrimitives(type: CircleInfo.primitiveType, indexCount: 28 * 3, indexType: .uint16, indexBuffer:CircleInfo.indexBuffer, indexBufferOffset: 0)
     }
     
