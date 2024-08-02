962c0ab55730bdf8a03a75ca310144a69f5606c4
yukiny0811 <yukiny0811@gmail.com>
2024-08-03 04:04:41 +0900

    update easy metal shader version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.2"),
     .package(url: "https://github.com/yukiny0811/SimpleSimdSwift", from: "1.0.1"),
     .package(url: "https://github.com/yukiny0811/FontVertexBuilder", from: "1.2.1"),
-    .package(url: "https://github.com/yukiny0811/EasyMetalShader.git", from: "3.1.1"),
+    .package(url: "https://github.com/yukiny0811/EasyMetalShader.git", from: "3.2.0"),
     .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
 ]
 
