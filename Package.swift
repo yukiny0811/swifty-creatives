0aa97719028c5234d0a9cb42c431c2cb5fe244b6
Yuki Kuwashima <yukiny0811@gmail.com>
2025-01-31 17:12:04 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/yukiny0811/SimpleSimdSwift", exact: "1.0.1"),
     .package(url: "https://github.com/yukiny0811/FontVertexBuilder", exact: "1.2.5"),
     .package(url: "https://github.com/yukiny0811/EasyMetalShader.git", exact: "3.2.1"),
-    .package(url: "https://github.com/apple/swift-syntax.git", exact: "510.0.3"),
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
 ]
 
 enum CorePackage {
